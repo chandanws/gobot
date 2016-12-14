5fd58167cea58a248dfb954175eb371ae6ddfc05
Tomas Vik <tomas.vik@rea-group.com>
2016-12-14 18:37:53 +1100

    Added naive implementation of move

--- robot_engine.go
+++ robot_engine.go
@@ -1,12 +1,14 @@
 package gobot
 
 import "fmt"
+import "errors"
 
 type Command int
 type Direction int
 
 const (
 	PLACE Command = iota
+	MOVE  Command = iota
 )
 
 const (
@@ -36,9 +38,34 @@ type Robot struct {
 	facing Direction
 }
 
-func Run(table Table, command Command, x int, y int, direction Direction) (Robot, error) {
-	if x >= table.width || y >= table.height {
-		return *new(Robot), outOfBoundsError{table, x, y}
+func Run(table Table, command Command, x int, y int, direction Direction) (Table, error) {
+	switch command {
+	case PLACE:
+		if x >= table.width || y >= table.height {
+			return *new(Table), outOfBoundsError{table, x, y}
+		}
+		return Table{table.height, table.width, Robot{x, y, direction}, true}, nil
+	case MOVE:
+		movedX, movedY := move(table.robot.x, table.robot.y, table.robot.facing)
+		return Table{table.height, table.width, Robot{movedX, movedY, table.robot.facing}, true}, nil
+
+	default:
+		return *new(Table), errors.New("unrecognized cmmand")
 	}
-	return Robot{x, y, direction}, nil
+
+}
+
+func move(x int, y int, facing Direction) (int, int) {
+	switch facing {
+	case NORTH:
+		return x, y + 1
+	case EAST:
+		return x + 1, y
+	case SOUTH:
+		return x, y - 1
+	case WEST:
+		return x - 1, y
+	default:
+		panic("there is only 4 world directions")
+	}
 }
--- robot_engine_test.go
+++ robot_engine_test.go
@@ -2,12 +2,12 @@ package gobot
 
 import "testing"
 
-func TestPlace(t *testing.T) {
+func TestPlaceCommand(t *testing.T) {
 	table := Table{5, 5, *new(Robot), false}
-	robot, _ := Run(table, PLACE, 1, 2, SOUTH)
-	expectedRobot := Robot{1, 2, SOUTH}
-	if robot != expectedRobot {
-		t.Errorf("robot %+v is not equal %+v", robot, expectedRobot)
+	newTable, _ := Run(table, PLACE, 1, 2, SOUTH)
+	expectedTable := Table{5, 5, Robot{1, 2, SOUTH}, true}
+	if newTable != expectedTable {
+		t.Errorf("table %+v is not equal %+v", newTable, expectedTable)
 	}
 }
 
@@ -19,14 +19,39 @@ func TestPlaceOutOfBounds(t *testing.T) {
 	}
 }
 
-/*
-func TestMove(t *testing.T) {
-	table := Table{5, 5}
-	robot := Rorbot{1, 2, SOUTH}
-	newRobot := Run(&table, MOVE, robot)
-	expectedRobot := Robot{1, 1, SOUTH}
-	if newRobot != expectedRobot {
-		t.Errorf("robot %+v\n is not Robot{1,1,SOUTH}", robot)
+func TestMoveCommand(t *testing.T) {
+	table := Table{5, 5, Robot{1, 2, SOUTH}, true}
+	newTable, _ := Run(table, MOVE, 0, 0, 0)
+	expectedTable := Table{5, 5, Robot{1, 1, SOUTH}, true}
+	if newTable != expectedTable {
+		t.Errorf("table %+v is not equal %+v", newTable, expectedTable)
 	}
 }
-*/
+
+func TestMoveNorth(t *testing.T) {
+	x, y := move(0, 0, NORTH)
+	if x != 0 || y != 1 {
+		t.Errorf("moved to %d,%d instad of 0,1", x, y)
+	}
+}
+
+func TestMoveEast(t *testing.T) {
+	x, y := move(1, 1, EAST)
+	if x != 2 || y != 1 {
+		t.Errorf("moved to %d,%d instad of 0,1", x, y)
+	}
+}
+
+func TestMoveSouth(t *testing.T) {
+	x, y := move(2, 2, SOUTH)
+	if x != 2 || y != 1 {
+		t.Errorf("moved to %d,%d instad of 0,1", x, y)
+	}
+}
+
+func TestMoveWest(t *testing.T) {
+	x, y := move(1, 1, WEST)
+	if x != 0 || y != 1 {
+		t.Errorf("moved to %d,%d instad of 0,1", x, y)
+	}
+}
